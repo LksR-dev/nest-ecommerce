b76c99fb759d6e9541db6d68e29eac2d21811632
Lucas <lucasmruiz05@gmail.com>
2023-12-13 16:59:54 -0300

    first commit, base folders
    first commit

---
+++ src/app_module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ControllersModule } from './infrastructure/controllers/controllers_module';
+import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment_config_module';
+import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
+import { LoggerModule } from './infrastructure/logger/logger.module';
+import { typeOrmConfig } from './infrastructure/config/typeorm/typeorm_config';
+import { UsecasesProxyModule } from './infrastructure/usecases-proxy/usecases_module';
+import { SengridModule } from './infrastructure/common/sengrid/sengrid_module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule as JwtServiceModule } from './infrastructure/services/jwt/jwt_module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './infrastructure/common/strategies/jwt_strategy';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.secret,
+    }),
+    JwtServiceModule,
+    EnvironmentConfigModule,
+    UsecasesProxyModule.register(),
+    ControllersModule,
+    LoggerModule,
+    ExceptionsModule,
+    TypeOrmModule.forRoot(typeOrmConfig),
+    SengridModule,
+  ],
+  providers: [JwtStrategy],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app_module';
+
+import { AllExceptionFilter } from './infrastructure/common/filter/exception_filter';
+import { LoggerService } from './infrastructure/logger/logger.service';
+import { LoggingInterceptor } from './infrastructure/common/interceptors/logger_interceptor';
+import {
+  ResponseFormat,
+  ResponseInterceptor,
+} from './infrastructure/common/interceptors/response_interceptor';
+
+async function bootstrap() {
+  const env = process.env.NODE_ENV;
+  const app = await NestFactory.create(AppModule);
+
+  //Filter
+  app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+
+  //Pipes
+  app.useGlobalPipes(new ValidationPipe());
+
+  //Interceptors
+  app.useGlobalInterceptors(new LoggingInterceptor(new LoggerService()));
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
+  //Swagger config
+  if (env !== 'production') {
+    const config = new DocumentBuilder()
+      .addBearerAuth()
+      .setTitle('ecommerce')
+      .setDescription('ecommerce')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config, {
+      extraModels: [ResponseFormat],
+      deepScanRoutes: true,
+    });
+    SwaggerModule.setup('api', app, document);
+  }
+  await app.listen(3000);
+}
+bootstrap();
